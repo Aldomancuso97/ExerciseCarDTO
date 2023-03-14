3582824b9813888b9695adb664a833cf945c9f23
Aldo Mancuso <mancusoaldo8 @gmail.com>
2023-03-14 13:03:39 +0100

    first commit

---
+++ src/main/java/com/exercise/car/CarDTOApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDTOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDTOApplication.class, args);
+	}
+
+}
